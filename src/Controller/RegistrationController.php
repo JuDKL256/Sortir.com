f30d3a9fc465edbb5ea7ab40f2d5a719122525d5
jnagel2024 <jonathan_591@hotmail.fr>
2025-04-01 15:37:25 +0200

    Modification css

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Participant;
+
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
